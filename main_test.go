db96df3106a35956cd6d909c6c8a06803c2d80c1
Zach Latta <zach@zachlatta.com>
2014-12-13 15:25:41 -0800

    fix breaking tests

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 func TestGeocode(t *testing.T) {
 	query := "1600 amphitheatre parkway"
 	expectedAddress := "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA"
-	expectedLatitude, expectedLongitude := 37.4219998, -122.0839596
+	expectedLatitude, expectedLongitude := 37.422611, -122.0840577
 
 	result, err := geocode(query, new(geo.GoogleGeocoder))
 	if err != nil {
